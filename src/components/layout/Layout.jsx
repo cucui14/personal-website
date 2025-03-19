e3b856cf61b3c0f182acbcec23bac9d87d8c7c97
Angel Pureco <angelpureco@Angels-MacBook-Pro.local>
2025-03-19 17:56:16 -0600

    removed Header from Layout

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,10 +1,9 @@
-import Header from '../header/Header.jsx';
+
 import Footer from '../footer/Footer.jsx';
 
 const Layout = ({ children }) => {
     return (
         <div>
-            <Header />
             <main>{children}</main>
             <Footer />
         </div>
