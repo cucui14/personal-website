d165933da661b740c673fbe5d3dc15eb1317e66a
Angel Pureco <angelpureco@Angels-MacBook-Pro.local>
2025-03-19 17:58:25 -0600

    removed Footer from Layout

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,11 +1,7 @@
-
-import Footer from '../footer/Footer.jsx';
-
 const Layout = ({ children }) => {
     return (
         <div>
             <main>{children}</main>
-            <Footer />
         </div>
     )  
 }
